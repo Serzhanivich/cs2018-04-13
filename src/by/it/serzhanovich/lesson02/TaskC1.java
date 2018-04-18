1c61886368de466ec4926176ff5b95e7747c7448
serzhanovich <333antitrust33319732gmail.com>
2018-04-18 21:46:30 +0300

    ver2

--- src/by/it/serzhanovich/lesson02/TaskC1.java
+++ src/by/it/serzhanovich/lesson02/TaskC1.java
@@ -1,5 +1,7 @@
 package by.it.serzhanovich.lesson02;
 
+import java.util.Scanner;
+
 /* Нужно написать программу, которая вводит два числа с клавиатуры
 и выводит их сумму на экран в виде
 
@@ -10,6 +12,14 @@ package by.it.serzhanovich.lesson02;
 Sum = 26
 */
 class TaskC1 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println(a+b);
+    }
 
 
 
