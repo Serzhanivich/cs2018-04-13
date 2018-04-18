1763aff30205b1eb13c2ef88bf7c3a303328a28f
serzhanovich <333antitrust33319732gmail.com>
2018-04-18 21:46:30 +0300

    ver2

--- src/by/it/serzhanovich/lesson02/TaskB1.java
+++ src/by/it/serzhanovich/lesson02/TaskB1.java
@@ -16,10 +16,17 @@ Scanner sc=new Scanner(System.in);
 
  */
 
+
 import java.util.Scanner;
 
 class TaskB1 {
-
-
+    public static void main(String[] args) {
+        int a;
+        Scanner sc=new Scanner(System.in);
+a=sc.nextInt();
+        System.out.println(a*a);
     }
+
+
+ }
    
